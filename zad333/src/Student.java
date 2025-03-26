a80886f816bfe1566b754f2493c406579900554c
s33641 <s33641@pjwstk.edu.pl>
2025-03-27 00:41:39 +0100

    Update Student.java
    Error

--- zad333/src/Student.java
+++ zad333/src/Student.java
@@ -1,22 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class Student {
-        public String name;
-        public String fname;
-        public String lname;
-        public String indexnumber;
-        public String email;
-        public String address;
-        public List<Double> grades;
+    String firstName;
+    String lastName;
+    String indexNumber;
+    String email;
+    String address;
+    List<Double> grades = new ArrayList<>();
 
-
-        public Student(String fname, String lname, String indexnumber, String email, String address, List<Double> grades) {
-            this.fname = fname;
-            this.lname = lname;
-            this.indexnumber = indexnumber;
-            this.email = email;
-            this.address = address;
-            this.grades = new ArrayList<>();
+    public double calculateAverage() {
+        if (grades.isEmpty()) {
+            throw new RuntimeException("Student nie ma ocen.");
         }
+
+        double total = 0;
+        for (double grade : grades) {
+            total += grade;
+        }
+
+        double average = total / grades.size();
+        return Math.round(average * 2) / 2.0;
     }
 
+    public void addGrade(double grade) {
+        if (grades.size() >= 20) {
+            throw new RuntimeException("Maksymalna liczba ocen to 20.");
+        }
+        grades.add(grade);
+    }
+}

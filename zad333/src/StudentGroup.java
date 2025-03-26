bf64ba340b2bf700e804382b571cd8f7e842ed1b
s33641 <s33641@pjwstk.edu.pl>
2025-03-27 00:41:56 +0100

    Update StudentGroup.java
    error with commtiing on my pc

--- zad333/src/StudentGroup.java
+++ zad333/src/StudentGroup.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class StudentGroup {
-        public String name;
-        public List<Double> grades;
+public class studentgroup {
+    public String nazwa;
+    public List <String> sklad = new ArrayList<String>();
 
-        public StudentGroup(String name) {
-            this.name = name;
-            this.grades = new ArrayList<>();
+    public void addtogroup (String index){
+        if(sklad.size()>15){
+            throw new RuntimeException("max liczba studentow w grupie to 15");
         }
+        if(sklad.contains(index)){
+            throw new RuntimeException("ten student juz jest w grupie");
+        }
+        sklad.add(index);
     }
-
+}

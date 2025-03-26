f3779d9fd528f23934b79ae8781dc088bd504669
s33641 <s33641@pjwstk.edu.pl>
2025-03-27 00:41:21 +0100

    Update Main.java
    Error with pushing on my pc

--- zad333/src/Main.java
+++ zad333/src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+
+public class studentgroup {
+    public String nazwa;
+    public List <String> sklad = new ArrayList<String>();
+
+    public void addtogroup (String index){
+        if(sklad.size()>15){
+            throw new RuntimeException("max liczba studentow w grupie to 15");
+        }
+        if(sklad.contains(index)){
+            throw new RuntimeException("ten student juz jest w grupie");
+        }
+        sklad.add(index);
     }
 }
-
